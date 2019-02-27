39a9cbb0635264455f1f0dc41e2cfa8585ee2419
ozturkosu <ozturk.osu@gmail.com>
2019-02-26 19:02:11 -0500

    a

--- MGRES/mgmres.hpp
+++ MGRES/mgmres.hpp
@@ -20,7 +20,7 @@ void mgmres_st ( int n, int nz_num, int ia[], int ja[], double a[], double x[],
   double rhs[], int itr_max, int mr, double tol_abs, double tol_rel );
 void mgmres_fault_st(int n, int nz_num, int ia[], int ja[], double a[], double x[],
 	double rhs[], int itr_max, int mr, double tol_abs, double tol_rel ,
-	int psize, double treshold, int fPos, int range1 , int range2, int k ) ;
+	int psize,  int fPos, int range1 , int range2, int kf ) ;
 void mult_givens ( double c, double s, int k, double g[] );
 void pmgmres_ilu_cr ( int n, int nz_num, int ia[], int ja[], double a[], 
   double x[], double rhs[], int itr_max, int mr, double tol_abs, 
