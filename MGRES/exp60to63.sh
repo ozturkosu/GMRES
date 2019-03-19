94ed129f0f260badf1154ebc0abe9d1da3f4de9f
ozturkosu <ozturk.osu@gmail.com>
2019-03-19 04:50:25 -0400

    sh file

---
+++ MGRES/exp60to63.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+mkdir intoX60to63rangeNis200
+./main 5 200 60 63 5 >> intoX60to63rangeNis200/result.txt
+mkdir intoX60to63rangeNis400
+./main 5 400 60 63 5 >> intoX60to63rangeNis400/result.txt
+mkdir intoX60to63rangeNis600
+./main 5 600 60 63 5 >> intoX60to63rangeNis600/result.txt
+mkdir intoX60to63rangeNis800
+./main 5 800 60 63 5 >> intoX60to63rangeNis800/result.txt
+mkdir intoY60to63rangeNis200
+./main 5 200 60 63 4 >> intoY60to63rangeNis200/result.txt
+mkdir intoY0to63rangeNis400
+./main 5 400 60 63 4 >> intoY60to63rangeNis400/result.txt
+mkdir intoY60to63rangeNis600
+./main 5 600 60 63 4 >> intoY60to63rangeNis600/result.txt
+mkdir intoY60to63rangeNis800
+./main 5 800 60 63 4 >> intoY60to63rangeNis800/result.txt
+mkdir intoH60to63rangeNis200
+./main 5 200 60 63 3 >> intoH60to63rangeNis200/result.txt
+mkdir intoH60to63rangeNis400
+./main 5 400 60 63 3 >> intoH60to63rangeNis400/result.txt
+mkdir intoH60to63rangeNis600
+./main 5 600 60 63 3 >> intoH60to63rangeNis600/result.txt
+mkdir intoH60to63rangeNis800
+./main 5 800 60 63 3 >> intoH60to63rangeNis800/result.txt
+mkdir intoV60to63rangeNis200
+./main 5 200 60 63 2 >> intoV60to63rangeNis200/result.txt
+mkdir intoV60to63rangeNis400
+./main 5 400 60 63 2 >> intoV60to63rangeNis400/result.txt
+mkdir intoV60to63rangeNis600
+./main 5 600 60 63 2 >> intoV60to63rangeNis600/result.txt
+mkdir intoV60to63rangeNis800
+./main 5 800 60 63 2 >> intoV60to63rangeNis800/result.txt
+mkdir intoS60to63rangeNis200
+./main 5 200 60 63 1 >> intoS60to63rangeNis200/result.txt
+mkdir intoS60to63rangeNis400
+./main 5 400 60 63 1 >> intoS60to63rangeNis400/result.txt
+mkdir intoS60to63rangeNis600
+./main 5 600 60 63 1 >> intoS60to63rangeNis600/result.txt
+mkdir intoS60to63rangeNis800
+./main 5 800 60 63 1 >> intoS60to63rangeNis800/result.txt
+mkdir intoR60to63rangeNis200
+./main 5 200 60 63 0 >> intoR60to63rangeNis200/result.txt
+mkdir intoR60to63rangeNis400
+./main 5 400 60 63 0 >> intoR60to63rangeNis400/result.txt
+mkdir intoR60to63rangeNis600
+./main 5 600 60 63 0 >> intoR60to63rangeNis600/result.txt
+mkdir intoR60to63rangeNis800
+./main 5 800 60 63 0 >> intoR60to63rangeNis800/result.txt
