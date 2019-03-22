5822c72bfe87535f70010b09c7eaba709728927c
ozturkosu <ozturk.osu@gmail.com>
2019-03-22 05:40:19 -0400

    new script

---
+++ MGRES/innerExp1000simple56to59.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+mkdir intoX56to59rangeNis200
+./main4 5 100 56 59 5 494_bus.mtx>> intoX56to59rangeNis200/inner56to59result_simp_1000.txt
+mkdir intoX56to59rangeNis400
+./main4 5 200 56 59 5 494_bus.mtx>> intoX56to59rangeNis400/inner56to59result_simp_1000.txt
+mkdir intoX56to59rangeNis600
+./main4 5 300 56 59 5 494_bus.mtx>> intoX56to59rangeNis600/inner56to59result_simp_1000.txt
+mkdir intoX56to59rangeNis800
+./main4 5 400 56 59 5 494_bus.mtx>> intoX56to59rangeNis800/inner56to59result_simp_1000.txt
+mkdir intoY56to59rangeNis200
+./main4 5 100 56 59 4 494_bus.mtx>> intoY56to59rangeNis200/inner56to59result_simp_1000.txt
+mkdir intoY56to59rangeNis400
+./main4 5 200 56 59 4 494_bus.mtx>> intoY56to59rangeNis400/inner56to59result_simp_1000.txt
+mkdir intoY56to59rangeNis600
+./main4 5 300 56 59 4 494_bus.mtx>> intoY56to59rangeNis600/inner56to59result_simp_1000.txt
+mkdir intoY56to59rangeNis800
+./main4 5 400 56 59 4 494_bus.mtx>> intoY56to59rangeNis800/inner56to59result_simp_1000.txt
+mkdir intoH56to59rangeNis200
+./main4 5 100 56 59 3 494_bus.mtx >> intoH56to59rangeNis200/inner56to59result_simp_1000.txt
+mkdir intoH56to59rangeNis400
+./main4 5 200 56 59 3 494_bus.mtx >> intoH56to59rangeNis400/inner56to59result_simp_1000.txt
+mkdir intoH56to59rangeNis600
+./main4 5 300 56 59 3 494_bus.mtx >> intoH56to59rangeNis600/inner56to59result_simp_1000.txt
+mkdir intoH56to59rangeNis800
+./main4 5 400 56 59 3 494_bus.mtx >> intoH56to59rangeNis800/inner56to59result_simp_1000.txt
+mkdir intoV56to59rangeNis200
+./main4 5 100 56 59 2 494_bus.mtx >> intoV56to59rangeNis200/inner56to59result_simp_1000.txt
+mkdir intoV56to59rangeNis400
+./main4 5 200 56 59 2 494_bus.mtx >> intoV56to59rangeNis400/inner56to59result_simp_1000.txt
+mkdir intoV56to59rangeNis600
+./main4 5 300 56 59 2 494_bus.mtx >> intoV56to59rangeNis600/inner56to59result_simp_1000.txt
+mkdir intoV56to59rangeNis800
+./main4 5 400 56 59 2 494_bus.mtx >> intoV56to59rangeNis800/inner56to59result_simp_1000.txt
+mkdir intoS56to59rangeNis200
+./main4 5 100 56 59 1 494_bus.mtx >> intoS56to59rangeNis200/inner56to59result_simp_1000.txt
+mkdir intoS56to59rangeNis400
+./main4 5 200 56 59 1 494_bus.mtx >> intoS56to59rangeNis400/inner56to59result_simp_1000.txt
+mkdir intoS56to59rangeNis600
+./main4 5 300 56 59 1 494_bus.mtx >> intoS56to59rangeNis600/inner56to59result_simp_1000.txt
+mkdir intoS56to59rangeNis800
+./main4 5 400 56 59 1 494_bus.mtx >> intoS56to59rangeNis800/inner56to59result_simp_1000.txt
+mkdir intoR56to59rangeNis200
+./main4 5 100 56 59  0 494_bus.mtx >> intoR56to59rangeNis200/inner56to59result_simp_1000.txt
+mkdir intoR56to59rangeNis400
+./main4 5 200 56 59 0 494_bus.mtx >> intoR56to59rangeNis400/inner56to59result_simp_1000.txt
+mkdir intoR56to59rangeNis600
+./main4 5 300 56 59 0 494_bus.mtx >> intoR56to59rangeNis600/inner56to59result_simp_1000.txt
+mkdir intoR56to59rangeNis800
+./main4 5 400 56 59 0 494_bus.mtx >> intoR56to59rangeNis800/inner56to59result_simp_1000.txt
