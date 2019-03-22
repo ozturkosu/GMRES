a83f9c654f16e994bfe3f5272206484d6dc5f9c7
ozturkosu <ozturk.osu@gmail.com>
2019-03-22 04:28:02 -0400

    k

--- MGRES/Rexp1000simple60to63.sh
+++ MGRES/Rexp1000simple60to63.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir intoR60to63rangeNis200
-./main3 5 60 59 0 494_bus.mtx >> intoR60to63rangeNis200/60to63result_simp_1000.txt
+./main3 5 60 60 63 0 494_bus.mtx >> intoR60to63rangeNis200/60to63result_simp_1000.txt
 mkdir intoR60to63rangeNis400
 ./main3 5 120 60 63 0 494_bus.mtx >> intoR60to63rangeNis400/60to63result_simp_1000.txt
 mkdir intoR60to63rangeNis600
--- MGRES/exp1000simple60to63.sh
+++ MGRES/exp1000simple60to63.sh
@@ -40,7 +40,7 @@ mkdir intoS60to63rangeNis600
 mkdir intoS60to63rangeNis800
 ./main3 5 240 60 63 1 494_bus.mtx >> intoS60to63rangeNis800/60to63result_simp_1000.txt
 mkdir intoR60to63rangeNis200
-./main3 5 60 59 0 494_bus.mtx >> intoR60to63rangeNis200/60to63result_simp_1000.txt
+./main3 5 60 60 63  0 494_bus.mtx >> intoR60to63rangeNis200/60to63result_simp_1000.txt
 mkdir intoR60to63rangeNis400
 ./main3 5 120 60 63 0 494_bus.mtx >> intoR60to63rangeNis400/60to63result_simp_1000.txt
 mkdir intoR60to63rangeNis600
