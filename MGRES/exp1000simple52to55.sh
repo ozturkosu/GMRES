6f76d7a6651b68e220f7ed7d028347c4474ff923
ozturkosu <ozturk.osu@gmail.com>
2019-03-22 00:38:28 -0400

    new script

---
+++ MGRES/exp1000simple52to55.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+mkdir intoX52to55rangeNis200
+./main3 5 200 52 55 5 494_bus.mtx>> intoX52to55rangeNis200/52to55result_simp_1000.txt
+mkdir intoX52to55rangeNis400
+./main3 5 400 52 55 5 494_bus.mtx>> intoX52to55rangeNis400/52to55result_simp_1000.txt
+mkdir intoX52to55rangeNis600
+./main3 5 600 52 55 5 494_bus.mtx>> intoX52to55rangeNis600/52to55result_simp_1000.txt
+mkdir intoX52to55rangeNis800
+./main3 5 800 52 55 5 494_bus.mtx>> intoX52to55rangeNis800/52to55result_simp_1000.txt
+mkdir intoY52to55rangeNis200
+./main3 5 200 52 55 4 494_bus.mtx>> intoY52to55rangeNis200/52to55result_simp_1000.txt
+mkdir intoY52to55rangeNis400
+./main3 5 400 52 55 4 494_bus.mtx>> intoY52to55rangeNis400/52to55result_simp_1000.txt
+mkdir intoY52to55rangeNis600
+./main3 5 600 52 55 4 494_bus.mtx>> intoY52to55rangeNis600/52to55result_simp_1000.txt
+mkdir intoY52to55rangeNis800
+./main3 5 800 52 55 4 494_bus.mtx>> intoY52to55rangeNis800/52to55result_simp_1000.txt
+mkdir intoH52to55rangeNis200
+./main3 5 200 52 55 3 494_bus.mtx >> intoH52to55rangeNis200/52to55result_simp_1000.txt
+mkdir intoH52to55rangeNis400
+./main3 5 400 52 55 3 494_bus.mtx >> intoH52to55rangeNis400/52to55result_simp_1000.txt
+mkdir intoH52to55rangeNis600
+./main3 5 600 52 55 3 494_bus.mtx >> intoH52to55rangeNis600/52to55result_simp_1000.txt
+mkdir intoH52to55rangeNis800
+./main3 5 800 52 55 3 494_bus.mtx >> intoH52to55rangeNis800/52to55result_simp_1000.txt
+mkdir intoV52to55rangeNis200
+./main3 5 200 52 55 2 494_bus.mtx >> intoV52to55rangeNis200/52to55result_simp_1000.txt
+mkdir intoV52to55rangeNis400
+./main3 5 400 52 55 2 494_bus.mtx >> intoV52to55rangeNis400/52to55result_simp_1000.txt
+mkdir intoV52to55rangeNis600
+./main3 5 600 52 55 2 494_bus.mtx >> intoV52to55rangeNis600/52to55result_simp_1000.txt
+mkdir intoV52to55rangeNis800
+./main3 5 800 52 55 2 494_bus.mtx >> intoV52to55rangeNis800/52to55result_simp_1000.txt
+mkdir intoS52to55rangeNis200
+./main3 5 200 52 55 1 494_bus.mtx >> intoS52to55rangeNis200/52to55result_simp_1000.txt
+mkdir intoS52to55rangeNis400
+./main3 5 400 52 55 1 494_bus.mtx >> intoS52to55rangeNis400/52to55result_simp_1000.txt
+mkdir intoS52to55rangeNis600
+./main3 5 600 52 55 1 494_bus.mtx >> intoS52to55rangeNis600/52to55result_simp_1000.txt
+mkdir intoS52to55rangeNis800
+./main3 5 800 52 55 1 494_bus.mtx >> intoS52to55rangeNis800/52to55result_simp_1000.txt
+mkdir intoR52to55rangeNis200
+./main3 5 200 52 55 0 494_bus.mtx >> intoR52to55rangeNis200/52to55result_simp_1000.txt
+mkdir intoR52to55rangeNis400
+./main3 5 400 52 55 0 494_bus.mtx >> intoR52to55rangeNis400/52to55result_simp_1000.txt
+mkdir intoR52to55rangeNis600
+./main3 5 600 52 55 0 494_bus.mtx >> intoR52to55rangeNis600/52to55result_simp_1000.txt
+mkdir intoR52to55rangeNis800
+./main3 5 800 52 55 0 494_bus.mtx >> intoR52to55rangeNis800/52to55result_simp_1000.txt
