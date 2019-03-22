338cff8ac915dc6ca2db65d0aa069c3121cc4fe6
ozturkosu <ozturk.osu@gmail.com>
2019-03-22 00:18:43 -0400

    simple

---
+++ MGRES/exp1000simple60to63.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+mkdir intoX60to63rangeNis200
+./main3 5 200 60 63 5 494_bus.mtx>> intoX60to63rangeNis200/60to63result_simple_1000.txt
+mkdir intoX60to63rangeNis400
+./main3 5 400 60 63 5 494_bus.mtx>> intoX60to63rangeNis400/60to63result_simple_1000.txt
+mkdir intoX60to63rangeNis600
+./main3 5 600 60 63 5 494_bus.mtx>> intoX60to63rangeNis600/60to63result_simple_1000.txt
+mkdir intoX60to63rangeNis800
+./main3 5 800 60 63 5 494_bus.mtx>> intoX60to63rangeNis800/60to63result_simple_1000.txt
+mkdir intoY60to63rangeNis200
+./main3 5 200 60 63 4 494_bus.mtx>> intoY60to63rangeNis200/60to63result_simple_1000.txt
+mkdir intoY60to63rangeNis400
+./main3 5 400 60 63 4 494_bus.mtx>> intoY60to63rangeNis400/60to63result_simple_1000.txt
+mkdir intoY60to63rangeNis600
+./main3 5 600 60 63 4 494_bus.mtx>> intoY60to63rangeNis600/60to63result_simple_1000.txt
+mkdir intoY60to63rangeNis800
+./main3 5 800 60 63 4 494_bus.mtx>> intoY60to63rangeNis800/60to63result_simple_1000.txt
+mkdir intoH60to63rangeNis200
+./main3 5 200 60 63 3 494_bus.mtx >> intoH60to63rangeNis200/60to63result_simple_1000.txt
+mkdir intoH60to63rangeNis400
+./main3 5 400 60 63 3 494_bus.mtx >> intoH60to63rangeNis400/60to63result_simple_1000.txt
+mkdir intoH60to63rangeNis600
+./main3 5 600 60 63 3 494_bus.mtx >> intoH60to63rangeNis600/60to63result_simple_1000.txt
+mkdir intoH60to63rangeNis800
+./main3 5 800 60 63 3 494_bus.mtx >> intoH60to63rangeNis800/60to63result_simple_1000.txt
+mkdir intoV60to63rangeNis200
+./main3 5 200 60 63 2 494_bus.mtx >> intoV60to63rangeNis200/60to63result_simple_1000.txt
+mkdir intoV60to63rangeNis400
+./main3 5 400 60 63 2 494_bus.mtx >> intoV60to63rangeNis400/60to63result_simple_1000.txt
+mkdir intoV60to63rangeNis600
+./main3 5 600 60 63 2 494_bus.mtx >> intoV60to63rangeNis600/60to63result_simple_1000.txt
+mkdir intoV60to63rangeNis800
+./main3 5 800 60 63 2 494_bus.mtx >> intoV60to63rangeNis800/60to63result_simple_1000.txt
+mkdir intoS60to63rangeNis200
+./main3 5 200 60 63 1 494_bus.mtx >> intoS60to63rangeNis200/60to63result_simple_1000.txt
+mkdir intoS60to63rangeNis400
+./main3 5 400 60 63 1 494_bus.mtx >> intoS60to63rangeNis400/60to63result_simple_1000.txt
+mkdir intoS60to63rangeNis600
+./main3 5 600 60 63 1 494_bus.mtx >> intoS60to63rangeNis600/60to63result_simple_1000.txt
+mkdir intoS60to63rangeNis800
+./main3 5 800 60 63 1 494_bus.mtx >> intoS60to63rangeNis800/60to63result_simple_1000.txt
+mkdir intoR60to63rangeNis200
+./main3 5 200 60 63 0 494_bus.mtx >> intoR60to63rangeNis200/60to63result_simple_1000.txt
+mkdir intoR60to63rangeNis400
+./main3 5 400 60 63 0 494_bus.mtx >> intoR60to63rangeNis400/60to63result_simple_1000.txt
+mkdir intoR60to63rangeNis600
+./main3 5 600 60 63 0 494_bus.mtx >> intoR60to63rangeNis600/60to63result_simple_1000.txt
+mkdir intoR60to63rangeNis800
+./main3 5 800 60 63 0 494_bus.mtx >> intoR60to63rangeNis800/60to63result_simple_1000.txt
