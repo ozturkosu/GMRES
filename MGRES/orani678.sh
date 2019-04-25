f6b43f0c810c4c9167c9c2200ba792731ab358f8
ozturkosu <ozturk.osu@gmail.com>
2019-04-25 16:20:16 -0400

    orani

---
+++ MGRES/orani678.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#SBATCH --time=23:59:00
+mkdir intoV60to63rangeNis200
+./main6 5 400 60 63 6 2000 orani678.mtx>> intoV60to63rangeNis200/result_orani678.txt
+mkdir intoV60to63rangeNis400
+./main6 5 800 60 63 6 2000 orani678.mtx>> intoV60to63rangeNis400/result_orani678.txt
+mkdir intoV60to63rangeNis600
+./main6 5 1200 60 63 6 2000 orani678.mtx>> intoV60to63rangeNis600/result_orani678.txt
+mkdir intoV60to63rangeNis800
+./main6 5 1600 60 63 6 2000 orani678.mtx>> intoV60to63rangeNis800/result_orani678.txt
+mkdir intoX60to63rangeNis200
+./main6 5 400 60 63 5 2000 orani678.mtx>> intoX60to63rangeNis200/result_orani678.txt
+mkdir intoX60to63rangeNis400
+./main6 5 800 60 63 5 2000 orani678.mtx>> intoX60to63rangeNis400/result_orani678.txt
+mkdir intoX60to63rangeNis600
+./main6 5 1200 60 63 5 2000 orani678.mtx>> intoX60to63rangeNis600/result_orani678.txt
+mkdir intoX60to63rangeNis800
+./main6 5 1600 60 63 5 2000 orani678.mtx>> intoX60to63rangeNis800/result_orani678.txt
+mkdir intoY60to63rangeNis200
+./main6 5 400 60 63 4 2000 orani678.mtx>> intoY60to63rangeNis200/result_orani678.txt
+mkdir intoY60to63rangeNis400
+./main6 5 800 60 63 4 2000 orani678.mtx>> intoY60to63rangeNis400/result_orani678.txt
+mkdir intoY60to63rangeNis600
+./main6 5 1200 60 63 4 2000 orani678.mtx>> intoY60to63rangeNis600/result_orani678.txt
+mkdir intoY60to63rangeNis800
+./main6 5 1600 60 63 4 2000 orani678.mtx>> intoY60to63rangeNis800/result_orani678.txt
+mkdir intoH60to63rangeNis200
+./main6 5 400 60 63 3 2000 orani678.mtx >> intoH60to63rangeNis200/result_orani678.txt
+mkdir intoH60to63rangeNis400
+./main6 5 800 60 63 3 2000 orani678.mtx >> intoH60to63rangeNis400/result_orani678.txt
+mkdir intoH60to63rangeNis600
+./main6 5 1200 60 63 3 2000 orani678.mtx >> intoH60to63rangeNis600/result_orani678.txt
+mkdir intoH60to63rangeNis800
+./main6 5 1600 60 63 3 2000 orani678.mtx >> intoH60to63rangeNis800/result_orani678.txt
+mkdir intoC60to63rangeNis200
+./main6 5 400 60 63 2 2000 orani678.mtx >> intoC60to63rangeNis200/result_orani678.txt
+mkdir intoC60to63rangeNis400
+./main6 5 800 60 63 2 2000 orani678.mtx >> intoC60to63rangeNis400/result_orani678.txt
+mkdir intoC60to63rangeNis600
+./main6 5 1200 60 63 2 2000 orani678.mtx >> intoC60to63rangeNis600/result_orani678.txt
+mkdir intoC60to63rangeNis800
+./main6 5 1600 60 63 2 2000 orani678.mtx >> intoC60to63rangeNis800/result_orani678.txt
+mkdir intoS60to63rangeNis200
+./main6 5 400 60 63 1 2000 orani678.mtx >> intoS60to63rangeNis200/result_orani678.txt
+mkdir intoS60to63rangeNis400
+./main6 5 800 60 63 1 2000 orani678.mtx >> intoS60to63rangeNis400/result_orani678.txt
+mkdir intoS60to63rangeNis600
+./main6 5 1200 60 63 1 2000 orani678.mtx >> intoS60to63rangeNis600/result_orani678.txt
+mkdir intoS60to63rangeNis800
+./main6 5 1600 60 63 1 2000 orani678.mtx >> intoS60to63rangeNis800/result_orani678.txt
+mkdir intoR60to63rangeNis200
+./main6 5 400 60 63 0 2000 orani678.mtx >> intoR60to63rangeNis200/result_orani678.txt
+mkdir intoR60to63rangeNis400
+./main6 5 800 60 63 0 2000 orani678.mtx >> intoR60to63rangeNis400/result_orani678.txt
+mkdir intoR60to63rangeNis600
+./main6 5 1200 60 63 0 2000 orani678.mtx >> intoR60to63rangeNis600/result_orani678.txt
+mkdir intoR60to63rangeNis800
+./main6 5 1600 60 63 0 2000 orani678.mtx >> intoR60to63rangeNis800/result_orani678.txt
