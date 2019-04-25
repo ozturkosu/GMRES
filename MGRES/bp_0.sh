0d3c364d38745691ea6c2c36a9ae95871d1a6dea
ozturkosu <ozturk.osu@gmail.com>
2019-04-25 16:15:05 -0400

    new

---
+++ MGRES/bp_0.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#SBATCH --time=23:59:00
+mkdir intoV60to63rangeNis200
+./main6 5 80 60 63 6 400 bp_0.mtx>> intoV60to63rangeNis200/2_result_bp_0.txt
+mkdir intoV60to63rangeNis400
+./main6 5 160 60 63 6 400 bp_0.mtx>> intoV60to63rangeNis400/2_result_bp_0.txt
+mkdir intoV60to63rangeNis600
+./main6 5 240 60 63 6 400 bp_0.mtx>> intoV60to63rangeNis600/2_result_bp_0.txt
+mkdir intoV60to63rangeNis800
+./main6 5 320 60 63 6 400 bp_0.mtx>> intoV60to63rangeNis800/2_result_bp_0.txt
+mkdir intoX60to63rangeNis200
+./main6 5 80 60 63 5 400 bp_0.mtx>> intoX60to63rangeNis200/2_result_bp_0.txt
+mkdir intoX60to63rangeNis400
+./main6 5 160 60 63 5 400 bp_0.mtx>> intoX60to63rangeNis400/2_result_bp_0.txt
+mkdir intoX60to63rangeNis600
+./main6 5 240 60 63 5 400 bp_0.mtx>> intoX60to63rangeNis600/2_result_bp_0.txt
+mkdir intoX60to63rangeNis800
+./main6 5 320 60 63 5 400 bp_0.mtx>> intoX60to63rangeNis800/2_result_bp_0.txt
+mkdir intoY60to63rangeNis200
+./main6 5 80 60 63 4 400 bp_0.mtx>> intoY60to63rangeNis200/2_result_bp_0.txt
+mkdir intoY60to63rangeNis400
+./main6 5 160 60 63 4 400 bp_0.mtx>> intoY60to63rangeNis400/2_result_bp_0.txt
+mkdir intoY60to63rangeNis600
+./main6 5 240 60 63 4 400 bp_0.mtx>> intoY60to63rangeNis600/2_result_bp_0.txt
+mkdir intoY60to63rangeNis800
+./main6 5 320 60 63 4 400 bp_0.mtx>> intoY60to63rangeNis800/2_result_bp_0.txt
+mkdir intoH60to63rangeNis200
+./main6 5 80 60 63 3 400 bp_0.mtx >> intoH60to63rangeNis200/2_result_bp_0.txt
+mkdir intoH60to63rangeNis400
+./main6 5 160 60 63 3 400 bp_0.mtx >> intoH60to63rangeNis400/2_result_bp_0.txt
+mkdir intoH60to63rangeNis600
+./main6 5 240 60 63 3 400 bp_0.mtx >> intoH60to63rangeNis600/2_result_bp_0.txt
+mkdir intoH60to63rangeNis800
+./main6 5 320 60 63 3 400 bp_0.mtx >> intoH60to63rangeNis800/2_result_bp_0.txt
+mkdir intoC60to63rangeNis200
+./main6 5 80 60 63 2 400 bp_0.mtx >> intoC60to63rangeNis200/2_result_bp_0.txt
+mkdir intoC60to63rangeNis400
+./main6 5 160 60 63 2 400 bp_0.mtx >> intoC60to63rangeNis400/2_result_bp_0.txt
+mkdir intoC60to63rangeNis600
+./main6 5 240 60 63 2 400 bp_0.mtx >> intoC60to63rangeNis600/2_result_bp_0.txt
+mkdir intoC60to63rangeNis800
+./main6 5 320 60 63 2 400 bp_0.mtx >> intoC60to63rangeNis800/2_result_bp_0.txt
+mkdir intoS60to63rangeNis200
+./main6 5 80 60 63 1 400 bp_0.mtx >> intoS60to63rangeNis200/2_result_bp_0.txt
+mkdir intoS60to63rangeNis400
+./main6 5 160 60 63 1 400 bp_0.mtx >> intoS60to63rangeNis400/2_result_bp_0.txt
+mkdir intoS60to63rangeNis600
+./main6 5 240 60 63 1 400 bp_0.mtx >> intoS60to63rangeNis600/2_result_bp_0.txt
+mkdir intoS60to63rangeNis800
+./main6 5 320 60 63 1 400 bp_0.mtx >> intoS60to63rangeNis800/2_result_bp_0.txt
+mkdir intoR60to63rangeNis200
+./main6 5 80 60 63 0 400 bp_0.mtx >> intoR60to63rangeNis200/2_result_bp_0.txt
+mkdir intoR60to63rangeNis400
+./main6 5 160 60 63 0 400 bp_0.mtx >> intoR60to63rangeNis400/2_result_bp_0.txt
+mkdir intoR60to63rangeNis600
+./main6 5 240 60 63 0 400 bp_0.mtx >> intoR60to63rangeNis600/2_result_bp_0.txt
+mkdir intoR60to63rangeNis800
+./main6 5 320 60 63 0 400 bp_0.mtx >> intoR60to63rangeNis800/2_result_bp_0.txt
