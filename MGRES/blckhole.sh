957ee3667506f685f70daf767995a397888728f8
ozturkosu <ozturk.osu@gmail.com>
2019-04-25 16:06:55 -0400

    new script

---
+++ MGRES/blckhole.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#SBATCH --time=23:59:00
+mkdir intoV60to63rangeNis200
+./main6 5 400 60 63 6 2000 blckhole.mtx>> intoV60to63rangeNis200/result_blckhole.txt
+mkdir intoV60to63rangeNis400
+./main6 5 800 60 63 6 2000 blckhole.mtx>> intoV60to63rangeNis400/result_blckhole.txt
+mkdir intoV60to63rangeNis600
+./main6 5 1200 60 63 6 2000 blckhole.mtx>> intoV60to63rangeNis600/result_blckhole.txt
+mkdir intoV60to63rangeNis800
+./main6 5 1600 60 63 6 2000 blckhole.mtx>> intoV60to63rangeNis800/result_blckhole.txt
+mkdir intoX60to63rangeNis200
+./main6 5 400 60 63 5 2000 blckhole.mtx>> intoX60to63rangeNis200/result_blckhole.txt
+mkdir intoX60to63rangeNis400
+./main6 5 800 60 63 5 2000 blckhole.mtx>> intoX60to63rangeNis400/result_blckhole.txt
+mkdir intoX60to63rangeNis600
+./main6 5 1200 60 63 5 2000 blckhole.mtx>> intoX60to63rangeNis600/result_blckhole.txt
+mkdir intoX60to63rangeNis800
+./main6 5 1600 60 63 5 2000 blckhole.mtx>> intoX60to63rangeNis800/result_blckhole.txt
+mkdir intoY60to63rangeNis200
+./main6 5 400 60 63 4 2000 blckhole.mtx>> intoY60to63rangeNis200/result_blckhole.txt
+mkdir intoY60to63rangeNis400
+./main6 5 800 60 63 4 2000 blckhole.mtx>> intoY60to63rangeNis400/result_blckhole.txt
+mkdir intoY60to63rangeNis600
+./main6 5 1200 60 63 4 2000 blckhole.mtx>> intoY60to63rangeNis600/result_blckhole.txt
+mkdir intoY60to63rangeNis800
+./main6 5 1600 60 63 4 2000 blckhole.mtx>> intoY60to63rangeNis800/result_blckhole.txt
+mkdir intoH60to63rangeNis200
+./main6 5 400 60 63 3 2000 blckhole.mtx >> intoH60to63rangeNis200/result_blckhole.txt
+mkdir intoH60to63rangeNis400
+./main6 5 800 60 63 3 2000 blckhole.mtx >> intoH60to63rangeNis400/result_blckhole.txt
+mkdir intoH60to63rangeNis600
+./main6 5 1200 60 63 3 2000 blckhole.mtx >> intoH60to63rangeNis600/result_blckhole.txt
+mkdir intoH60to63rangeNis800
+./main6 5 1600 60 63 3 2000 blckhole.mtx >> intoH60to63rangeNis800/result_blckhole.txt
+mkdir intoC60to63rangeNis200
+./main6 5 400 60 63 2 2000 blckhole.mtx >> intoC60to63rangeNis200/result_blckhole.txt
+mkdir intoC60to63rangeNis400
+./main6 5 800 60 63 2 2000 blckhole.mtx >> intoC60to63rangeNis400/result_blckhole.txt
+mkdir intoC60to63rangeNis600
+./main6 5 1200 60 63 2 2000 blckhole.mtx >> intoC60to63rangeNis600/result_blckhole.txt
+mkdir intoC60to63rangeNis800
+./main6 5 1600 60 63 2 2000 blckhole.mtx >> intoC60to63rangeNis800/result_blckhole.txt
+mkdir intoS60to63rangeNis200
+./main6 5 400 60 63 1 2000 blckhole.mtx >> intoS60to63rangeNis200/result_blckhole.txt
+mkdir intoS60to63rangeNis400
+./main6 5 800 60 63 1 2000 blckhole.mtx >> intoS60to63rangeNis400/result_blckhole.txt
+mkdir intoS60to63rangeNis600
+./main6 5 1200 60 63 1 2000 blckhole.mtx >> intoS60to63rangeNis600/result_blckhole.txt
+mkdir intoS60to63rangeNis800
+./main6 5 1600 60 63 1 2000 blckhole.mtx >> intoS60to63rangeNis800/result_blckhole.txt
+mkdir intoR60to63rangeNis200
+./main6 5 400 60 63 0 2000 blckhole.mtx >> intoR60to63rangeNis200/result_blckhole.txt
+mkdir intoR60to63rangeNis400
+./main6 5 800 60 63 0 2000 blckhole.mtx >> intoR60to63rangeNis400/result_blckhole.txt
+mkdir intoR60to63rangeNis600
+./main6 5 1200 60 63 0 2000 blckhole.mtx >> intoR60to63rangeNis600/result_blckhole.txt
+mkdir intoR60to63rangeNis800
+./main6 5 1600 60 63 0 2000 blckhole.mtx >> intoR60to63rangeNis800/result_blckhole.txt
