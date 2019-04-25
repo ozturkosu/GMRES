e803ce62806bc3d12e019ead5c769cebab6a1246
ozturkosu <ozturk.osu@gmail.com>
2019-04-25 15:58:11 -0400

    new

---
+++ MGRES/bcsstm27.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#SBATCH --time=23:59:00
+mkdir intoV60to63rangeNis200
+./main6 5 200 60 63 6 1000 bcsstm27.mtx>> intoV60to63rangeNis200/result_bcsstm27.txt
+mkdir intoV60to63rangeNis400
+./main6 5 400 60 63 6 1000 bcsstm27.mtx>> intoV60to63rangeNis400/result_bcsstm27.txt
+mkdir intoV60to63rangeNis600
+./main6 5 600 60 63 6 1000 bcsstm27.mtx>> intoV60to63rangeNis600/result_bcsstm27.txt
+mkdir intoV60to63rangeNis800
+./main6 5 800 60 63 6 1000 bcsstm27.mtx>> intoV60to63rangeNis800/result_bcsstm27.txt
+mkdir intoX60to63rangeNis200
+./main6 5 200 60 63 5 1000 bcsstm27.mtx>> intoX60to63rangeNis200/result_bcsstm27.txt
+mkdir intoX60to63rangeNis400
+./main6 5 400 60 63 5 1000 bcsstm27.mtx>> intoX60to63rangeNis400/result_bcsstm27.txt
+mkdir intoX60to63rangeNis600
+./main6 5 600 60 63 5 1000 bcsstm27.mtx>> intoX60to63rangeNis600/result_bcsstm27.txt
+mkdir intoX60to63rangeNis800
+./main6 5 800 60 63 5 1000 bcsstm27.mtx>> intoX60to63rangeNis800/result_bcsstm27.txt
+mkdir intoY60to63rangeNis200
+./main6 5 200 60 63 4 1000 bcsstm27.mtx>> intoY60to63rangeNis200/result_bcsstm27.txt
+mkdir intoY60to63rangeNis400
+./main6 5 400 60 63 4 1000 bcsstm27.mtx>> intoY60to63rangeNis400/result_bcsstm27.txt
+mkdir intoY60to63rangeNis600
+./main6 5 600 60 63 4 1000 bcsstm27.mtx>> intoY60to63rangeNis600/result_bcsstm27.txt
+mkdir intoY60to63rangeNis800
+./main6 5 800 60 63 4 1000 bcsstm27.mtx>> intoY60to63rangeNis800/result_bcsstm27.txt
+mkdir intoH60to63rangeNis200
+./main6 5 200 60 63 3 1000 bcsstm27.mtx >> intoH60to63rangeNis200/result_bcsstm27.txt
+mkdir intoH60to63rangeNis400
+./main6 5 400 60 63 3 1000 bcsstm27.mtx >> intoH60to63rangeNis400/result_bcsstm27.txt
+mkdir intoH60to63rangeNis600
+./main6 5 600 60 63 3 1000 bcsstm27.mtx >> intoH60to63rangeNis600/result_bcsstm27.txt
+mkdir intoH60to63rangeNis800
+./main6 5 800 60 63 3 1000 bcsstm27.mtx >> intoH60to63rangeNis800/result_bcsstm27.txt
+mkdir intoC60to63rangeNis200
+./main6 5 200 60 63 2 1000 bcsstm27.mtx >> intoC60to63rangeNis200/result_bcsstm27.txt
+mkdir intoC60to63rangeNis400
+./main6 5 400 60 63 2 1000 bcsstm27.mtx >> intoC60to63rangeNis400/result_bcsstm27.txt
+mkdir intoC60to63rangeNis600
+./main6 5 600 60 63 2 1000 bcsstm27.mtx >> intoC60to63rangeNis600/result_bcsstm27.txt
+mkdir intoC60to63rangeNis800
+./main6 5 800 60 63 2 1000 bcsstm27.mtx >> intoC60to63rangeNis800/result_bcsstm27.txt
+mkdir intoS60to63rangeNis200
+./main6 5 200 60 63 1 1000 bcsstm27.mtx >> intoS60to63rangeNis200/result_bcsstm27.txt
+mkdir intoS60to63rangeNis400
+./main6 5 400 60 63 1 1000 bcsstm27.mtx >> intoS60to63rangeNis400/result_bcsstm27.txt
+mkdir intoS60to63rangeNis600
+./main6 5 600 60 63 1 1000 bcsstm27.mtx >> intoS60to63rangeNis600/result_bcsstm27.txt
+mkdir intoS60to63rangeNis800
+./main6 5 800 60 63 1 1000 bcsstm27.mtx >> intoS60to63rangeNis800/result_bcsstm27.txt
+mkdir intoR60to63rangeNis200
+./main6 5 200 60 63 0 1000 bcsstm27.mtx >> intoR60to63rangeNis200/result_bcsstm27.txt
+mkdir intoR60to63rangeNis400
+./main6 5 400 60 63 0 1000 bcsstm27.mtx >> intoR60to63rangeNis400/result_bcsstm27.txt
+mkdir intoR60to63rangeNis600
+./main6 5 600 60 63 0 1000 bcsstm27.mtx >> intoR60to63rangeNis600/result_bcsstm27.txt
+mkdir intoR60to63rangeNis800
+./main6 5 800 60 63 0 1000 bcsstm27.mtx >> intoR60to63rangeNis800/result_bcsstm27.txt
