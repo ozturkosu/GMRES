596ec39e1419fb2883665f5252d98fb2aadcde5e
ozturkosu <ozturk.osu@gmail.com>
2019-06-11 22:30:51 -0400

    new sh

---
+++ 56bcspwr05mtx.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#SBATCH --time=23:59:00
+mkdir intoV56to59rangeNis200
+./main6 5 100 56 59 6 400 bcspwr05.mtx>> intoV56to59rangeNis200/56_result_bcspwr05.txt
+mkdir intoV56to59rangeNis400
+./main6 5 200 56 59 6 400 bcspwr05.mtx>> intoV56to59rangeNis400/56_result_bcspwr05.txt
+mkdir intoV56to59rangeNis600
+./main6 5 300 56 59 6 400 bcspwr05.mtx>> intoV56to59rangeNis600/56_result_bcspwr05.txt
+mkdir intoV56to59rangeNis800
+./main6 5 400 56 59 6 400 bcspwr05.mtx>> intoV56to59rangeNis800/56_result_bcspwr05.txt
+mkdir intoX56to59rangeNis200
+./main6 5 100 56 59 5 400 bcspwr05.mtx>> intoX56to59rangeNis200/56_result_bcspwr05.txt
+mkdir intoX56to59rangeNis400
+./main6 5 200 56 59 5 400 bcspwr05.mtx>> intoX56to59rangeNis400/56_result_bcspwr05.txt
+mkdir intoX56to59rangeNis600
+./main6 5 300 56 59 5 400 bcspwr05.mtx>> intoX56to59rangeNis600/56_result_bcspwr05.txt
+mkdir intoX56to59rangeNis800
+./main6 5 400 56 59 5 400 bcspwr05.mtx>> intoX56to59rangeNis800/56_result_bcspwr05.txt
+mkdir intoY56to59rangeNis200
+./main6 5 100 56 59 4 400 bcspwr05.mtx>> intoY56to59rangeNis200/56_result_bcspwr05.txt
+mkdir intoY56to59rangeNis400
+./main6 5 200 56 59 4 400 bcspwr05.mtx>> intoY56to59rangeNis400/56_result_bcspwr05.txt
+mkdir intoY56to59rangeNis600
+./main6 5 300 56 59 4 400 bcspwr05.mtx>> intoY56to59rangeNis600/56_result_bcspwr05.txt
+mkdir intoY56to59rangeNis800
+./main6 5 400 56 59 4 400 bcspwr05.mtx>> intoY56to59rangeNis800/56_result_bcspwr05.txt
+mkdir intoH56to59rangeNis200
+./main6 5 100 56 59 3 400 bcspwr05.mtx >> intoH56to59rangeNis200/56_result_bcspwr05.txt
+mkdir intoH56to59rangeNis400
+./main6 5 200 56 59 3 400 bcspwr05.mtx >> intoH56to59rangeNis400/56_result_bcspwr05.txt
+mkdir intoH56to59rangeNis600
+./main6 5 300 56 59 3 400 bcspwr05.mtx >> intoH56to59rangeNis600/56_result_bcspwr05.txt
+mkdir intoH56to59rangeNis800
+./main6 5 400 56 59 3 400 bcspwr05.mtx >> intoH56to59rangeNis800/56_result_bcspwr05.txt
+mkdir intoC56to59rangeNis200
+./main6 5 100 56 59 2 400 bcspwr05.mtx >> intoC56to59rangeNis200/56_result_bcspwr05.txt
+mkdir intoC56to59rangeNis400
+./main6 5 200 56 59 2 400 bcspwr05.mtx >> intoC56to59rangeNis400/56_result_bcspwr05.txt
+mkdir intoC56to59rangeNis600
+./main6 5 300 56 59 2 400 bcspwr05.mtx >> intoC56to59rangeNis600/56_result_bcspwr05.txt
+mkdir intoC56to59rangeNis800
+./main6 5 400 56 59 2 400 bcspwr05.mtx >> intoC56to59rangeNis800/56_result_bcspwr05.txt
+mkdir intoS56to59rangeNis200
+./main6 5 100 56 59 1 400 bcspwr05.mtx >> intoS56to59rangeNis200/56_result_bcspwr05.txt
+mkdir intoS56to59rangeNis400
+./main6 5 200 56 59 1 400 bcspwr05.mtx >> intoS56to59rangeNis400/56_result_bcspwr05.txt
+mkdir intoS56to59rangeNis600
+./main6 5 300 56 59 1 400 bcspwr05.mtx >> intoS56to59rangeNis600/56_result_bcspwr05.txt
+mkdir intoS56to59rangeNis800
+./main6 5 400 56 59 1 400 bcspwr05.mtx >> intoS56to59rangeNis800/56_result_bcspwr05.txt
+mkdir intoR56to59rangeNis200
+./main6 5 100 56 59 0 400 bcspwr05.mtx >> intoR56to59rangeNis200/56_result_bcspwr05.txt
+mkdir intoR56to59rangeNis400
+./main6 5 200 56 59 0 400 bcspwr05.mtx >> intoR56to59rangeNis400/56_result_bcspwr05.txt
+mkdir intoR56to59rangeNis600
+./main6 5 300 56 59 0 400 bcspwr05.mtx >> intoR56to59rangeNis600/56_result_bcspwr05.txt
+mkdir intoR56to59rangeNis800
+./main6 5 400 56 59 0 400 bcspwr05.mtx >> intoR56to59rangeNis800/56_result_bcspwr05.txt
