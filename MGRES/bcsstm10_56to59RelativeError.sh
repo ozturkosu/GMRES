dc67727f4909848ddd86563d1dc7ce99e150e048
ozturkosu <ozturk.osu@gmail.com>
2019-12-10 08:24:58 -0500

    a

---
+++ MGRES/bcsstm10_56to59RelativeError.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --time=23:59:00
+mkdir intoY56to59rangeNis200
+./mainRelativeError 5 200 56 59 4 1000 bcsstm10.mtx>> intoY56to59rangeNis200/result_RelativeError_bcsstm10.txt
+mkdir intoY56to59rangeNis400
+./mainRelativeError 5 400 56 59 4 1000 bcsstm10.mtx>> intoY56to59rangeNis400/result_RelativeError_bcsstm10.txt
+mkdir intoY56to59rangeNis600
+./mainRelativeError 5 600 56 59 4 1000 bcsstm10.mtx>> intoY56to59rangeNis600/result_RelativeError_bcsstm10.txt
+mkdir intoY56to59rangeNis800
+./mainRelativeError 5 800 56 59 4 1000 bcsstm10.mtx>> intoY56to59rangeNis800/result_RelativeError_bcsstm10.txt
+mkdir intoC56to59rangeNis200
+./mainRelativeError 5 200 56 59 2 1000 bcsstm10.mtx >> intoC56to59rangeNis200/result_RelativeError_bcsstm10.txt
+mkdir intoC56to59rangeNis400
+./mainRelativeError 5 400 56 59 2 1000 bcsstm10.mtx >> intoC56to59rangeNis400/result_RelativeError_bcsstm10.txt
+mkdir intoC56to59rangeNis600
+./mainRelativeError 5 600 56 59 2 1000 bcsstm10.mtx >> intoC56to59rangeNis600/result_RelativeError_bcsstm10.txt
+mkdir intoC56to59rangeNis800
+./mainRelativeError 5 800 56 59 2 1000 bcsstm10.mtx >> intoC56to59rangeNis800/result_RelativeError_bcsstm10.txt
+mkdir intoS56to59rangeNis200
+./mainRelativeError 5 200 56 59 1 1000 bcsstm10.mtx >> intoS56to59rangeNis200/result_RelativeError_bcsstm10.txt
+mkdir intoS56to59rangeNis400
+./mainRelativeError 5 400 56 59 1 1000 bcsstm10.mtx >> intoS56to59rangeNis400/result_RelativeError_bcsstm10.txt
+mkdir intoS56to59rangeNis600
+./mainRelativeError 5 600 56 59 1 1000 bcsstm10.mtx >> intoS56to59rangeNis600/result_RelativeError_bcsstm10.txt
+mkdir intoS56to59rangeNis800
+./mainRelativeError 5 800 56 59 1 1000 bcsstm10.mtx >> intoS56to59rangeNis800/result_RelativeError_bcsstm10.txt
